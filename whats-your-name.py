4ab6f046ec6c626c93fbac12c79bf67a0a4c9374
Kai Wang <charles.wangkai@gmail.com>
2015-05-03 07:13:34 +0800

    What's your name

---
+++ whats-your-name.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+def solve():
+    first_name = input()
+    last_name = input()
+    print("Hello {first_name} {last_name}! You just delved into python.".format(first_name=first_name, last_name=last_name))
+
+if __name__ == "__main__":
+    solve()
