cee16a1654366c4c190c4a883925306da7092985
Kai Wang <charles.wangkai@gmail.com>
2015-05-04 03:24:29 +0800

    Map and Lambda Function

---
+++ map-and-lambda-expression.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+def solve():
+    N = int(input())
+    fibonacci = []
+    for i in range(N):
+        fibonacci.append(i if i < 2 else fibonacci[i - 2] + fibonacci[i - 1])
+    
+    print(list(map(lambda x: x ** 3, fibonacci)))
+    
+if __name__ == "__main__":
+    solve()
