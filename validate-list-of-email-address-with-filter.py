ee7981a8c91ce8f5b69e3bbefb83ad50486fbcf5
Kai Wang <charles.wangkai@gmail.com>
2015-05-04 04:06:36 +0800

    Validate list of email address with filter

---
+++ validate-list-of-email-address-with-filter.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import re
+
+def solve():
+    N = int(input())
+    emails = [input() for i in range(N)]
+    
+    print(sorted(list(filter(lambda email: re.fullmatch(r'[a-zA-Z0-9\-_]+@[a-zA-Z0-9]+\.[a-zA-Z0-9]{,3}', email) != None, emails))))
+    
+if __name__ == "__main__":
+    solve()
