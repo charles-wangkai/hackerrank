8d94f191c15ded2d8e707fe351fb902dc5fadca1
Kai Wang <charles.wangkai@gmail.com>
2015-05-04 04:35:50 +0800

    Find a string

---
+++ find-a-string.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+def solve():
+    string = input()
+    substring = input()
+    
+    occurrence = 0
+    start = 0
+    while True:
+        index = string.find(substring, start)
+        if index < 0:
+            break
+        occurrence += 1
+        start = index + 1
+        
+    print(occurrence)
+    
+if __name__ == "__main__":
+    solve()
