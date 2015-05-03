41ea3fca7048be4677a6ccc136eb3bb4bfbff139
Kai Wang <charles.wangkai@gmail.com>
2015-05-04 01:39:36 +0800

    List Comprehensions

---
+++ list-comprehensions.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+def solve():
+    X = int(input())
+    Y = int(input())
+    Z = int(input())
+    N = int(input())
+    
+    print([[x, y, z] for x in range(X + 1) for y in range(Y + 1) for z in range(Z + 1) if x + y + z != N])
+    
+if __name__ == "__main__":
+    solve()
