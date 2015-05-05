6087f0b6fea73289f7e04420380693aafbb34ce7
Kai Wang <charles.wangkai@gmail.com>
2015-05-05 20:10:36 +0800

    XML2 - Find the maximum depth

---
+++ xml2-find-the-maximum-depth.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import xml.etree.ElementTree as etree
+
+def find_depth(element):
+    depth = 0
+    for sub_element in element:
+        depth = max(depth, 1 + find_depth(sub_element))
+    return depth
+
+def solve():
+    line_num = int(input())
+    xml = ''
+    for i in range(line_num):
+        xml += input() + '\n'
+        
+    print(find_depth(etree.fromstring(xml)))
+    
+if __name__ == "__main__":
+    solve()
