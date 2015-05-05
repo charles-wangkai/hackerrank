c7ef46d20a867669e5438615b29e4b92321e8905
Kai Wang <charles.wangkai@gmail.com>
2015-05-06 03:43:55 +0800

    Standardize mobile number using Decorators

---
+++ standardize-mobile-number-using-decorators.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+def standardize_wrapper(func):
+    def standardize(mobile_number):
+        return "{prefix} {left} {right}".format(prefix='+91', left=mobile_number[-10:-5], right=mobile_number[-5:])
+    
+    def wrap(mobile_numbers):
+        return func(map(standardize, mobile_numbers))
+    
+    return wrap
+
+def solve():
+    N = int(input())
+    mobile_numbers = [input() for i in range(N)]
+    
+    standardized_sort = standardize_wrapper(sorted)
+    
+    for mobile_number in standardized_sort(mobile_numbers):
+        print(mobile_number)
+    
+if __name__ == "__main__":
+    solve()
