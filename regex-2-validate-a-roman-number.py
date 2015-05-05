5c31f782089de6e729a15a8e84e38204f43892ee
Kai Wang <charles.wangkai@gmail.com>
2015-05-05 15:55:01 +0800

    Regex 2 - Validate a Roman Number

---
+++ regex-2-validate-a-roman-number.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+import re
+
+def solve():
+    print("True" if re.fullmatch(r'(M{,3})?(D?C{,3}|CD|CM)?(L?X{,3}|XL|XC)?(V?I{,3}|IV|IX)?', input()) else "False")
+    
+if __name__ == "__main__":
+    solve()
