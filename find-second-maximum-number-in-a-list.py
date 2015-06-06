d5337ee7d24a9c57ffbd2e05e640c197aa0ee604
Kai Wang <charles.wangkai@gmail.com>
2015-06-06 21:19:20 +0800

    Find second largest number in a list [Rename to main()]

--- find-second-maximum-number-in-a-list.py
+++ find-second-maximum-number-in-a-list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
-def solve():
+def main():
     N = int(input())
     A = list(map(int, input().split()))
     
     print(sorted(set(A))[-2])
     
 if __name__ == "__main__":
-    solve()
+    main()
