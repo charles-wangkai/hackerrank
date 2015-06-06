ba2d2a30dd7d8beae5ec2850e49a21ae0f4e2bec
Kai Wang <charles.wangkai@gmail.com>
2015-06-06 21:40:58 +0800

    Regex: Validating Roman Numerals [Rename to main()]

--- regex-2-validate-a-roman-number.py
+++ regex-2-validate-a-roman-number.py
@@ -2,8 +2,8 @@
 
 import re
 
-def solve():
+def main():
     print("True" if re.fullmatch(r'(M{,3})?(D?C{,3}|CD|CM)?(L?X{,3}|XL|XC)?(V?I{,3}|IV|IX)?', input()) else "False")
     
 if __name__ == "__main__":
-    solve()
+    main()
