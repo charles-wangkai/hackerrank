606b3dc3d63ef1fe8738b44ea4f29748ee9ca6ca
Kai Wang <charles.wangkai@gmail.com>
2015-06-06 21:39:44 +0800

    Regex: Validating phone numbers [Rename to main()]

--- regex-1-validating-the-phone-number.py
+++ regex-1-validating-the-phone-number.py
@@ -2,10 +2,10 @@
 
 import re
 
-def solve():
+def main():
     N = int(input())
     for i in range(N):
         print("YES" if re.fullmatch(r'[789]\d{9}', input()) else "NO")
     
 if __name__ == "__main__":
-    solve()
+    main()
