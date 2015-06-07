033d232105963f1de15b8909658f9057b2af6381
Kai Wang <charles.wangkai@gmail.com>
2015-06-07 15:05:42 +0800

    Integers Come In All Sizes

---
+++ python-integers-come-in-all-sizes.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python2.7
+
+from __future__ import division
+
+def main():
+    a = int(raw_input())
+    b = int(raw_input())
+    c = int(raw_input())
+    d = int(raw_input())
+    print a ** b + c ** d
+
+if __name__ == "__main__":
+    main()
