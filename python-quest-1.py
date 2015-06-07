39ea09ca6a156587e9fea6d06ac99debe0c453d5
Kai Wang <charles.wangkai@gmail.com>
2015-06-07 15:30:04 +0800

    Triangle Quest

---
+++ python-quest-1.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+def name():
+    for i in range(1, int(input())):  # More than 2 lines will result in 0 score. Do not leave a blank line also
+        print((10 ** i - 1) // 9 * i)
+
+if __name__ == '__main__':
+    name()
