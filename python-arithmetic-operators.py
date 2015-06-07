7cc491bca0bc5528ef752a09ef48bda561e2755e
Kai Wang <charles.wangkai@gmail.com>
2015-06-07 13:41:16 +0800

    Arithmetic Operators

---
+++ python-arithmetic-operators.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python2.7
+
+def main():
+    a = int(raw_input())
+    b = int(raw_input())
+    print a + b
+    print a - b
+    print a * b
+
+if __name__ == "__main__":
+    main()
