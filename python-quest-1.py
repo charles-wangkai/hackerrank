5ff724543ab0a22bffd44a63d7f55fcb326e05d4
Kai Wang <charles.wangkai@gmail.com>
2015-06-07 15:54:55 +0800

    Triangle Quest [Fix typo to rename to main()]

--- python-quest-1.py
+++ python-quest-1.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
-def name():
+def main():
     for i in range(1, int(input())):  # More than 2 lines will result in 0 score. Do not leave a blank line also
         print((10 ** i - 1) // 9 * i)
 
 if __name__ == '__main__':
-    name()
+    main()
