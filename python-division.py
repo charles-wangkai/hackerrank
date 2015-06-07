404fae8ff169a72e677d40c5f68a81aeb8cefd43
Kai Wang <charles.wangkai@gmail.com>
2015-06-07 13:49:27 +0800

    Python: Division

---
+++ python-division.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python2.7
+
+from __future__ import division
+
+def main():
+    a = int(raw_input())
+    b = int(raw_input())
+    print a // b
+    print a / b
+
+if __name__ == "__main__":
+    main()
