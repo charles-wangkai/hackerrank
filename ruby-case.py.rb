59baa7e8a786f5fa5126ac44aa1e56063c45305f
Kai Wang <charles.wangkai@gmail.com>
2015-06-13 04:18:05 +0800

    Ruby Control Structures - Case (Bonus Question)

---
+++ ruby-case.py.rb
@@ -0,0 +1,14 @@
+def identify_class(obj)
+  case obj
+  when Hacker
+    puts "It's a Hacker!"
+  when Submission
+    puts "It's a Submission!"
+  when TestCase
+    puts "It's a TestCase!"
+  when Contest
+    puts "It's a Contest!"
+  else
+    puts "It's an unknown model"
+  end
+end
