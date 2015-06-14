94be08611131920af5a5ac5a45be94bd2bd0da35
Kai Wang <charles.wangkai@gmail.com>
2015-06-15 02:53:41 +0800

    Solve me first

---
+++ solve-me-first/Solution.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Solution {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+
+		int A = in.nextInt();
+		int B = in.nextInt();
+		System.out.println(A + B);
+
+		in.close();
+	}
+}
