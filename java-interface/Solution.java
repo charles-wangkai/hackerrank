f0759ba1a0a95d23af1d7b5fd4c82c37308253d3
Kai Wang <charles.wangkai@gmail.com>
2015-06-28 20:06:55 +0800

    Java Interface

---
+++ java-interface/Solution.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+interface AdvancedArithmetic {
+	public abstract int divisorSum(int n);
+}
+
+class MyCalculator implements AdvancedArithmetic {
+	@Override
+	public int divisorSum(int n) {
+		int sum = 0;
+		for (int i = 1; i * i <= n; i++) {
+			if (n % i == 0) {
+				sum += i;
+				if (i * i != n) {
+					sum += n / i;
+				}
+			}
+		}
+		return sum;
+	}
+}
+
+public class Solution {
+	public static void main(String[] argh) {
+		MyCalculator my_calculator = new MyCalculator();
+		System.out.print("I implemented: ");
+		ImplementedInterfaceNames(my_calculator);
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		System.out.print(my_calculator.divisorSum(n) + "\n");
+
+	}
+
+	/*
+	 * ImplementedInterfaceNames method takes an object and prints the name of
+	 * the interfaces it implemented
+	 */
+	static void ImplementedInterfaceNames(Object o) {
+
+		Class[] theInterfaces = o.getClass().getInterfaces();
+		for (int i = 0; i < theInterfaces.length; i++) {
+			String interfaceName = theInterfaces[i].getName();
+			System.out.println(interfaceName);
+		}
+	}
+}
