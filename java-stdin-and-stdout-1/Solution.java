d5d26662f842466d04ee48c6b095e096044190bd
Kai Wang <charles.wangkai@gmail.com>
2015-08-21 15:15:36 +0800

    Java Stdin and Stdout 1

---
+++ java-stdin-and-stdout-1/Solution.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Solution {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+
+		for (int i = 0; i < 3; i++) {
+			System.out.println(in.nextInt());
+		}
+
+		in.close();
+	}
+}
