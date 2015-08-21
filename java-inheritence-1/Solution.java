50be9569c4ee0e6d9ad967bc4ed458ff982f01c8
Kai Wang <charles.wangkai@gmail.com>
2015-08-21 15:39:06 +0800

    Java Inheritence 1

---
+++ java-inheritence-1/Solution.java
@@ -0,0 +1,24 @@
+class Animal {
+	void walk() {
+		System.out.println("I am walking");
+	}
+}
+
+class Bird extends Animal {
+	void fly() {
+		System.out.println("I am flying");
+	}
+
+	void sing() {
+		System.out.println("I am singing");
+	}
+}
+
+public class Solution {
+	public static void main(String[] args) {
+		Bird bird = new Bird();
+		bird.walk();
+		bird.fly();
+		bird.sing();
+	}
+}
