87ba610e9a40b1af3504d8af9e7a46f6221ed201
Kai Wang <charles.wangkai@gmail.com>
2015-09-07 15:23:35 +0800

    Read Single Number

---
+++ read-single-number-input/Solution.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Solution {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+
+		System.out.println(10 + in.nextInt());
+
+		in.close();
+	}
+}
