c388278db3a4e782df8a44836866b7cce4972022
Kai Wang <charles.wangkai@gmail.com>
2015-10-03 11:24:01 +0800

    Polynomial Regression: Office Prices

---
+++ predicting-office-space-price.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+# http://scikit-learn.org/stable/modules/linear_model.html#polynomial-regression-extending-linear-models-with-basis-functions
+
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+import numpy as np
+
+def main():
+    F, N = map(int, input().split())
+    featuresList = []
+    targets = []
+    for _ in range(N):
+        fields = list(map(float, input().split()))
+        featuresList.append(fields[:-1])
+        targets.append(fields[-1])
+     
+    poly = PolynomialFeatures(degree=3)
+
+    regression = LinearRegression()
+    regression.fit(poly.fit_transform(np.array(featuresList)), targets)
+    
+    T = int(input())
+    for _ in range(T):
+        features = list(map(float, input().split()))
+        print(regression.predict(poly.fit_transform(np.array(features)))[0])
+
+if __name__ == "__main__":
+    main()
