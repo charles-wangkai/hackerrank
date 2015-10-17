7a9d31f2ee55319ec7f8aa99b2aa5883e86c8733
Kai Wang <charles.wangkai@gmail.com>
2015-10-17 11:11:22 +0800

    String Validators

---
+++ string-validators.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+def main():
+    S = input()
+    
+    print(any(map(str.isalnum, S)))
+    print(any(map(str.isalpha, S)))
+    print(any(map(str.isdigit, S)))
+    print(any(map(str.islower, S)))
+    print(any(map(str.isupper, S)))
+
+if __name__ == "__main__":
+    main()
