2539e1c165974c498c652c6dff16806140a45968
Kai Wang <charles.wangkai@gmail.com>
2015-10-17 10:38:46 +0800

    sWAP cASE

---
+++ swap-case.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+def swap_case(letter):
+    if letter.isupper():
+        return letter.lower()
+    if letter.islower():
+        return letter.upper()
+    return letter
+
+def main():
+    S = input()
+    
+    print(''.join(map(swap_case, S)))
+
+if __name__ == "__main__":
+    main()
