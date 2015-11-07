5a1bbfd286b170acff5c968b9f91e85273ef9c1b
Kai Wang <charles.wangkai@gmail.com>
2015-11-07 18:55:57 +0800

    Prime Checker

---
+++ prime-checker/Solution.java
@@ -0,0 +1,69 @@
+import static java.lang.System.in;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.lang.reflect.Method;
+import java.util.HashSet;
+import java.util.Set;
+
+class Prime {
+	void checkPrime(int... numbers) {
+		StringBuilder sb = new StringBuilder();
+		for (int number : numbers) {
+			if (isPrime(number)) {
+				if (sb.length() != 0) {
+					sb.append(" ");
+				}
+				sb.append(number);
+			}
+		}
+		System.out.println(sb);
+	}
+
+	boolean isPrime(int number) {
+		if (number < 2) {
+			return false;
+		}
+
+		for (int i = 2; i * i <= number; i++) {
+			if (number % i == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
+
+public class Solution {
+	public static void main(String[] args) {
+		try {
+			BufferedReader br = new BufferedReader(new InputStreamReader(in));
+			int n1 = Integer.parseInt(br.readLine());
+			int n2 = Integer.parseInt(br.readLine());
+			int n3 = Integer.parseInt(br.readLine());
+			int n4 = Integer.parseInt(br.readLine());
+			int n5 = Integer.parseInt(br.readLine());
+			Prime ob = new Prime();
+			ob.checkPrime(n1);
+			ob.checkPrime(n1, n2);
+			ob.checkPrime(n1, n2, n3);
+			ob.checkPrime(n1, n2, n3, n4, n5);
+			Method[] methods = Prime.class.getDeclaredMethods();
+			Set<String> set = new HashSet<>();
+			boolean overload = false;
+			for (int i = 0; i < methods.length; i++) {
+				if (set.contains(methods[i].getName())) {
+					overload = true;
+					break;
+				}
+				set.add(methods[i].getName());
+
+			}
+			if (overload) {
+				throw new Exception("Overloading not allowed");
+			}
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+}
