6551338d6ab9e919c021c97a32568a08ee196868
Kai Wang <charles.wangkai@gmail.com>
2015-11-12 21:53:43 +0800

    Java Regex 2 - Duplicate Words

---
+++ duplicate-word/DuplicateWords.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class DuplicateWords {
+	public static void main(String[] args) {
+
+		String pattern = "(?<!\\w)(\\w+)( \\1)*(?!\\w)";
+		Pattern r = Pattern.compile(pattern, Pattern.CASE_INSENSITIVE);
+
+		Scanner in = new Scanner(System.in);
+		int testCases = Integer.parseInt(in.nextLine());
+		while (testCases > 0) {
+			String input = in.nextLine();
+			Matcher m = r.matcher(input);
+			boolean findMatch = true;
+			while (m.find()) {
+				input = input.replaceAll(String.format("(?<!\\w)%s(?!\\w)", m.group()), m.group(1));
+				findMatch = false;
+			}
+			System.out.println(input);
+			testCases--;
+		}
+	}
+}
