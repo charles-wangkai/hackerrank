d6be11d89e75030260d1e659b28a834d69c0a41e
Kai Wang <charles.wangkai@gmail.com>
2015-11-14 03:53:30 +0800

    Word Order

---
+++ word-order.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+def main():
+    n = int(input())
+    
+    words = []
+    word2count = {}
+    for _ in range(n):
+        word = input()
+        if word in word2count:
+            word2count[word] += 1
+        else:
+            words.append(word)
+            word2count[word] = 1
+    
+    print(len(words))
+    print(*map(lambda word: word2count[word], words))
+
+if __name__ == "__main__":
+    main()
