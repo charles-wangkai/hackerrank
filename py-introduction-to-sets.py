0b46db86fc25d3b0f7df0ac143cc8fbf7c53ac4e
Kai Wang <charles.wangkai@gmail.com>
2015-11-15 17:49:22 +0800

    Introduction to Sets

---
+++ py-introduction-to-sets.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+def main():
+    height_num = int(input())
+    distinct_heights = set(map(int, input().split()))
+    
+    print(sum(distinct_heights) / len(distinct_heights))
+
+if __name__ == "__main__":
+    main()
