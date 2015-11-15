4a683e9e1eacfd49cf16da448684e256a5693fdc
Kai Wang <charles.wangkai@gmail.com>
2015-11-15 17:54:21 +0800

    Set .add()

---
+++ py-set-add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+def main():
+    N = int(input())
+    distinct_countries = set()
+    
+    for _ in range(N):
+        country = input()
+        distinct_countries.add(country)
+    
+    print(len(distinct_countries))
+
+if __name__ == "__main__":
+    main()
