27d04539b669c001e2e2930d7791640332ef9e43
Kai Wang <charles.wangkai@gmail.com>
2015-11-17 21:05:56 +0800

    Find Angle MBC

---
+++ find-angle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python2.7
+# -*- coding: utf-8 -*-
+
+import math
+
+def main():
+    AB = int(input())
+    BC = int(input())
+    
+    theta = round(math.degrees(math.asin(AB / math.sqrt(AB ** 2 + BC ** 2))))
+    print '{theta:.0f}°'.format(theta=theta)
+
+if __name__ == "__main__":
+    main()
