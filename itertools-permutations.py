c8a3b9434c035abb52fee120f990cf4e64020b1e
Kai Wang <charles.wangkai@gmail.com>
2015-11-19 19:07:45 +0800

    itertools.permutations()

---
+++ itertools-permutations.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import itertools
+
+def main():
+    S, k = input().split()
+    k = int(k)
+    
+    print(*map(lambda permutation: ''.join(permutation), itertools.permutations(sorted(S), k)), sep='\n')
+
+if __name__ == "__main__":
+    main()
