0ab4a7ba6a344940b3ac3e73ea5d632b241222f5
Kai Wang <charles.wangkai@gmail.com>
2015-11-22 18:19:18 +0800

    itertools.combinations()

---
+++ itertools-combinations.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import itertools
+
+def main():
+    S, k = input().split()
+    k = int(k)
+    
+    for i in range(1, k + 1):
+        print(*map(lambda combination: ''.join(combination), itertools.combinations(sorted(S), i)), sep='\n')
+
+if __name__ == "__main__":
+    main()
