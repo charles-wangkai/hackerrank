e44806981ce3166619a696a920c8cdd9dd04c540
Kai Wang <charles.wangkai@gmail.com>
2015-11-23 16:39:45 +0800

    Maximize It!

---
+++ maximize-it.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import itertools
+
+def read_numbers():
+    return map(int, input().split())
+
+def main():
+    K, M = read_numbers()
+    lists = [list(read_numbers())[1:] for _ in range(K)]
+    
+    print(max(map(lambda combination: sum(map(lambda x: x * x, combination)) % M, itertools.product(*lists))))
+
+if __name__ == "__main__":
+    main()
