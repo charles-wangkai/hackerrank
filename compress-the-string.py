ff97945a6d1f3b6714832720e61752d306c56118
Kai Wang <charles.wangkai@gmail.com>
2015-12-09 19:09:28 +0800

    Compress the String!

---
+++ compress-the-string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import itertools
+
+def main():
+    S = input()
+    
+    print(*(itertools.starmap(lambda key, group: (len(list(group)), int(key)), itertools.groupby(S))))
+
+if __name__ == "__main__":
+    main()
