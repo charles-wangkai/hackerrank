d308ac595fa7b6181e0f5485f6c7f56505ecabf4
Kai Wang <charles.wangkai@gmail.com>
2015-12-18 04:10:00 +0800

    Most Commons

---
+++ most-commons.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import collections
+
+def main():
+    S = input()
+    
+    for item in sorted(collections.Counter(S).items(), key=lambda item: (-item[1], item[0]))[:3]:
+        print(*item)
+
+if __name__ == "__main__":
+    main()
