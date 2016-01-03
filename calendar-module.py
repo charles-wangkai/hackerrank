6fd5b25713863ea02a7e78aa279f93c8c927ffc0
Kai Wang <charles.wangkai@gmail.com>
2016-01-03 23:36:35 +0800

    Calendar Module

---
+++ calendar-module.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import calendar
+
+def main():
+    month, day, year = map(int, input().split())
+    
+    print(calendar.day_name[calendar.weekday(year, month, day)].upper())
+
+if __name__ == "__main__":
+    main()
