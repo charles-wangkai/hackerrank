64c2bd4341ef3924cca69dca32b0f143eca39f4c
Kai Wang <charles.wangkai@gmail.com>
2016-04-18 00:04:04 +0800

    Day 0: Hello, World.

---
+++ 30-hello-world/Solution.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		String inputString = sc.nextLine();
+
+		System.out.println("Hello, World.");
+		System.out.println(inputString);
+
+		sc.close();
+	}
+}
