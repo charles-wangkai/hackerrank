881b75d2bdc1f58f508d689f34721bff1de96c15
Kai Wang <charles.wangkai@gmail.com>
2016-05-01 21:46:55 +0800

    Security Functions II

---
+++ security-function-ii/Solution.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int x = sc.nextInt();
+		System.out.println(x * x);
+
+		sc.close();
+	}
+}
