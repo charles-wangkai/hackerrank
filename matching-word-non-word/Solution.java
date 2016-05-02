355c5948748aec54ee6c7b33be1f09eaa6a53f77
Kai Wang <charles.wangkai@gmail.com>
2016-05-02 23:36:49 +0800

    Matching Word & Non-Word Character

---
+++ matching-word-non-word/Solution.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Solution {
+
+	public static void main(String[] args) {
+
+		Regex_Test tester = new Regex_Test();
+		tester.checker("\\w\\w\\w\\W\\w\\w\\w\\w\\w\\w\\w\\w\\w\\w\\W\\w\\w\\w"); // Use
+																					// \\
+																					// instead
+																					// of
+																					// using
+																					// \
+
+	}
+}
+
+class Regex_Test {
+
+	public void checker(String Regex_Pattern) {
+
+		Scanner Input = new Scanner(System.in);
+		String Test_String = Input.nextLine();
+		Pattern p = Pattern.compile(Regex_Pattern);
+		Matcher m = p.matcher(Test_String);
+		System.out.println(m.find());
+
+		Input.close();
+	}
+
+}
