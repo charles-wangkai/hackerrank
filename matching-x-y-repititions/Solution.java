ffd7dab8bd3c6216e8beacfe1e6fed2066a9dbe8
Kai Wang <charles.wangkai@gmail.com>
2016-05-07 23:57:51 +0800

    Matching {x, y} Repetitions

---
+++ matching-x-y-repititions/Solution.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Solution {
+
+	public static void main(String[] args) {
+
+		Regex_Test tester = new Regex_Test();
+		tester.checker("^\\d{1,2}\\p{Alpha}{3,}\\.{0,3}$"); // Use \\ instead of
+															// using \
+
+	}
+}
+
+class Regex_Test {
+
+	public void checker(String Regex_Pattern) {
+
+		Scanner Input = new Scanner(System.in);
+		String Test_String = Input.nextLine();
+		Pattern p = Pattern.compile(Regex_Pattern);
+		Matcher m = p.matcher(Test_String);
+		System.out.println(m.find());
+
+		Input.close();
+	}
+
+}
