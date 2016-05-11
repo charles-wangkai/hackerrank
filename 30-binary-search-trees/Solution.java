0393b111efc1ae1035ba1b8f388368aeeda6f9a9
Kai Wang <charles.wangkai@gmail.com>
2016-05-12 00:49:19 +0800

    Day 22: Binary Search Trees

---
+++ 30-binary-search-trees/Solution.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+class Node {
+	Node left, right;
+	int data;
+
+	Node(int data) {
+		this.data = data;
+		left = right = null;
+	}
+}
+
+public class Solution {
+	public static int getHeight(Node root) {
+		// Write your code here
+		if (root == null) {
+			return -1;
+		}
+		return 1 + Math.max(getHeight(root.left), getHeight(root.right));
+	}
+
+	public static Node insert(Node root, int data) {
+		if (root == null) {
+			return new Node(data);
+		} else {
+			Node cur;
+			if (data <= root.data) {
+				cur = insert(root.left, data);
+				root.left = cur;
+			} else {
+				cur = insert(root.right, data);
+				root.right = cur;
+			}
+			return root;
+		}
+	}
+
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		Node root = null;
+		while (T-- > 0) {
+			int data = sc.nextInt();
+			root = insert(root, data);
+		}
+		int height = getHeight(root);
+		System.out.println(height);
+
+		sc.close();
+	}
+}
