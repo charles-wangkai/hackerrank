b9db2fbb8a2204e28112be303f48ac5964764e91
Kai Wang <charles.wangkai@gmail.com>
2016-05-14 13:47:43 +0800

    Day 26: Nested Logic

---
+++ 30-nested-logic/Solution.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int actualDay = sc.nextInt();
+		int actualMonth = sc.nextInt();
+		int actualYear = sc.nextInt();
+		int expectedDay = sc.nextInt();
+		int expectedMonth = sc.nextInt();
+		int expectedYear = sc.nextInt();
+
+		int fine;
+		if (actualYear > expectedYear) {
+			fine = 10000;
+		} else if (actualMonth > expectedMonth) {
+			fine = 500 * (actualMonth - expectedMonth);
+		} else if (actualDay > expectedDay) {
+			fine = 15 * (actualDay - expectedDay);
+		} else {
+			fine = 0;
+		}
+		System.out.println(fine);
+
+		sc.close();
+	}
+}
