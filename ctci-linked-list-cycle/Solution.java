4b49e311ea20c4877adc06ed0102458e28c5eb13
Kai Wang <charles.wangkai@gmail.com>
2016-10-01 21:22:03 +0800

    Linked Lists: Detect a Cycle

---
+++ ctci-linked-list-cycle/Solution.java
@@ -0,0 +1,23 @@
+// Detect a cycle in a linked list. Note that the head pointer may be 'null' if the list is empty.
+
+// A Node is defined as: 
+class Node {
+	int data;
+	Node next;
+}
+
+public class Solution {
+	boolean hasCycle(Node head) {
+		Node slow = head;
+		Node fast = head;
+		while (fast != null && fast.next != null) {
+			slow = slow.next;
+			fast = fast.next.next;
+
+			if (slow == fast) {
+				return true;
+			}
+		}
+		return false;
+	}
+}
