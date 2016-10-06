9035436595d01fcba9f58adef95f73d870f5ac5b
Kai Wang <charles.wangkai@gmail.com>
2016-10-06 23:22:32 +0800

    Java SHA-256 [Remove unused code]

--- sha-256/Solution.java
+++ sha-256/Solution.java
@@ -14,16 +14,4 @@ public class Solution {
 
 		sc.close();
 	}
-
-	static String padZeros(int size, String str) {
-		return repeat('0', size - str.length()) + str;
-	}
-
-	static String repeat(char ch, int number) {
-		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < number; i++) {
-			sb.append(ch);
-		}
-		return sb.toString();
-	}
 }
