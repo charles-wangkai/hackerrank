96215ba534a02767098676529709d9a7ab56a721
Kai Wang <charles.wangkai@gmail.com>
2017-03-29 15:11:37 +0800

    Army Game

---
+++ game-with-cells/Solution.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		System.out.println(solve(n, m));
+
+		sc.close();
+	}
+
+	static int solve(int n, int m) {
+		return ((n + 1) / 2) * ((m + 1) / 2);
+	}
+}
