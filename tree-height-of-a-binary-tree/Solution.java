8265b36c52d726c8f7088b59d61b95bc3de4faa8
Kai Wang <charles.wangkai@gmail.com>
2017-08-29 14:39:33 +0800

    Tree: Height of a Binary Tree

---
+++ tree-height-of-a-binary-tree/Solution.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+class Node {
+	Node left;
+	Node right;
+	int data;
+
+	Node(int data) {
+		this.data = data;
+		left = null;
+		right = null;
+	}
+}
+
+public class Solution {
+	// class Node
+	// int data;
+	// Node left;
+	// Node right;
+
+	static int height(Node root) {
+		// Write your code here.
+		if (root == null) {
+			return -1;
+		}
+		return 1 + Math.max(height(root.left), height(root.right));
+	}
+
+	public static Node insert(Node root, int data) {
+		if (root == null) {
+			return new Node(data);
+		} else {
+			Node cur;
+			if (data <= root.data) {
+				cur = insert(root.left, data);
+				root.left = cur;
+			} else {
+				cur = insert(root.right, data);
+				root.right = cur;
+			}
+			return root;
+		}
+	}
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int t = scan.nextInt();
+		Node root = null;
+		while (t-- > 0) {
+			int data = scan.nextInt();
+			root = insert(root, data);
+		}
+		scan.close();
+		int height = height(root);
+		System.out.println(height);
+	}
+}
