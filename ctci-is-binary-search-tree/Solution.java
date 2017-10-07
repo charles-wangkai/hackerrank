d3594aa9108342268af8686a36d7cf642b53b1cc
Kai Wang <charles.wangkai@gmail.com>
2017-10-07 08:36:37 +0800

    Trees: Is This a Binary Search Tree? [Simplify by top-down search]

--- ctci-is-binary-search-tree/Solution.java
+++ ctci-is-binary-search-tree/Solution.java
@@ -9,44 +9,18 @@ class Node {
 
 public class Solution {
 	boolean checkBST(Node root) {
-		if (root == null) {
+		return check(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+	}
+
+	boolean check(Node node, int min, int max) {
+		if (node == null) {
 			return true;
 		}
 
-		return check(root).bst;
-	}
-
-	Result check(Node node) {
-		int min = node.data;
-		int max = node.data;
-
-		if (node.left != null) {
-			Result leftResult = check(node.left);
-			if (!leftResult.bst || leftResult.max >= node.data) {
-				return new Result(false, 0, 0);
-			}
-			min = leftResult.min;
-		}
-		if (node.right != null) {
-			Result rightResult = check(node.right);
-			if (!rightResult.bst || rightResult.min <= node.data) {
-				return new Result(false, 0, 0);
-			}
-			max = rightResult.max;
+		if (node.data < min || node.data > max) {
+			return false;
 		}
 
-		return new Result(true, min, max);
-	}
-}
-
-class Result {
-	boolean bst;
-	int min;
-	int max;
-
-	Result(boolean bst, int min, int max) {
-		this.bst = bst;
-		this.min = min;
-		this.max = max;
+		return check(node.left, min, node.data - 1) && check(node.right, node.data + 1, max);
 	}
 }
