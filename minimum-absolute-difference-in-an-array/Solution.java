fecbdcfc54a195162fcdf29c3061358500795ea6
Kai Wang <charles.wangkai@gmail.com>
2017-11-03 11:29:30 +0800

    Minimum Absolute Difference in an Array

---
+++ minimum-absolute-difference-in-an-array/Solution.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+		int[] a = new int[n];
+		for (int i = 0; i < a.length; i++) {
+			a[i] = sc.nextInt();
+		}
+		System.out.println(solve(a));
+
+		sc.close();
+	}
+
+	static int solve(int[] a) {
+		Arrays.sort(a);
+		return IntStream.range(0, a.length - 1).map(i -> a[i + 1] - a[i]).min().getAsInt();
+	}
+}
