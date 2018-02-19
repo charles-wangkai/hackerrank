89a2c9ad556937d58e27a84c5c96e5098e649760
Kai Wang <charles.wangkai@gmail.com>
2018-02-19 12:51:49 +0800

    Day 1: Functions

---
+++ js10-function.js
@@ -0,0 +1,37 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+
+/*
+ * Create the function factorial here
+ */
+function factorial(n) {
+    return (n == 0) ? 1 : (n * factorial(n - 1));
+}
+
+function main() {
+    const n = +(readLine());
+    
+    console.log(factorial(n));
+}
