970942f0f1c59d3c89d5ad94afb1e5c025af68b4
Kai Wang <charles.wangkai@gmail.com>
2018-02-19 13:06:33 +0800

    Day 1: Let and Const

---
+++ js10-let-and-const.js
@@ -0,0 +1,47 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function main() {
+    // Write your code here. Read input using 'readLine()' and print output using 'console.log()'.
+    const PI = Math.PI;
+
+    let r = Number.parseFloat(readLine());
+    let area = PI * r * r;
+    let perimeter = 2 * PI * r;
+    
+    // Print the area of the circle:
+    console.log(area);
+    
+    // Print the perimeter of the circle:
+    console.log(perimeter);
+    
+    try {    
+        // Attempt to redefine the value of constant variable PI
+        PI = 0;
+        // Attempt to print the value of PI
+        console.log(PI);
+    } catch(error) {
+        console.error("You correctly declared 'PI' as a constant.");
+    }
+}
