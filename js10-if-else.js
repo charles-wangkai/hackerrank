0fa9b85e3c72e74e9fb2134703e4c6d2d0c30ac5
Kai Wang <charles.wangkai@gmail.com>
2018-02-19 13:51:19 +0800

    Day 2: Conditional Statements: If-Else

---
+++ js10-if-else.js
@@ -0,0 +1,48 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function getGrade(score) {
+    let grade;
+    // Write your code here
+    if (score > 25 && score <= 30) {
+        grade = 'A';
+    } else if (score > 20 && score <= 25) {
+        grade = 'B';
+    } else if (score > 15 && score <= 20) {
+        grade = 'C';
+    } else if (score > 10 && score <= 15) {
+        grade = 'D';
+    } else if (score > 5 && score <= 10) {
+        grade = 'E';
+    } else if (score >= 0 && score <= 5) {
+        grade = 'F';
+    }
+    return grade;
+}
+
+function main() {
+    const score = +(readLine());
+    
+    console.log(getGrade(score));
+}
