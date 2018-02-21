a6591d1f76d2743e07d50eaf0cecde9c250d7983
Kai Wang <charles.wangkai@gmail.com>
2018-02-21 10:42:10 +0800

    Day 2: Conditional Statements: Switch

---
+++ js10-switch.js
@@ -0,0 +1,60 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function getLetter(s) {
+    let letter;
+    // Write your code here
+    switch (s[0]) {
+        case 'a':
+        case 'e':
+        case 'i':
+        case 'o':
+        case 'u':
+            letter = 'A';
+            break;
+        case 'b':
+        case 'c':
+        case 'd':
+        case 'f':
+        case 'g':
+            letter = 'B';
+            break;
+        case 'h':
+        case 'j':
+        case 'k':
+        case 'l':
+        case 'm':
+            letter = 'C';
+            break;
+        default:
+            letter = 'D';
+    }
+    return letter;
+}
+
+function main() {
+    const s = readLine();
+    
+    console.log(getLetter(s));
+}
