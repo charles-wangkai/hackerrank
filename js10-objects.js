b2b22b2f087ed0d6440e733fe78b7a0025ba6579
Kai Wang <charles.wangkai@gmail.com>
2018-07-31 12:57:28 +0800

    Day 4: Create a Rectangle Object

---
+++ js10-objects.js
@@ -0,0 +1,46 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+/*
+ * Complete the Rectangle function
+ */
+function Rectangle(a, b) {
+    return {
+        length: a,
+        width: b,
+        perimeter: 2 * (a + b),
+        area: a * b
+    }
+}
+
+function main() {
+    const a = +(readLine());
+    const b = +(readLine());
+    
+    const rec = new Rectangle(a, b);
+    
+    console.log(rec.length);
+    console.log(rec.width);
+    console.log(rec.perimeter);
+    console.log(rec.area);
+}
