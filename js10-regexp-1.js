972aa33928e6d8493835d74a701adeb8c92d7d16
Kai Wang <charles.wangkai@gmail.com>
2018-08-02 12:48:35 +0800

    Day 7: Regular Expressions I

---
+++ js10-regexp-1.js
@@ -0,0 +1,42 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+function regexVar() {
+    /*
+     * Declare a RegExp object variable named 're'
+     * It must match a string that starts and ends with the same vowel (i.e., {a, e, i, o, u})
+     */
+    let re = /^([aeiou]).*\1$/;
+    
+    /*
+     * Do not remove the return statement
+     */
+    return re;
+}
+
+function main() {
+    const re = regexVar();
+    const s = readLine();
+    
+    console.log(re.test(s));
+}
