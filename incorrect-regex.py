e3b6140fb2fe204c2574af1c02bd51feffee5800
Kai Wang <charles.wangkai@gmail.com>
2018-09-11 18:23:58 +0800

    Incorrect Regex

---
+++ incorrect-regex.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import re
+
+
+def solve(S):
+    try:
+        re.compile(S)
+        return True
+    except re.error:
+        return False
+
+
+def main():
+    T = int(input())
+    for _ in range(T):
+        S = input()
+        print(str(solve(S)))
+
+
+if __name__ == '__main__':
+    main()
