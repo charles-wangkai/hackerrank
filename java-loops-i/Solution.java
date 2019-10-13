812468ae5b4ca37f5ce19be95d90ef7357ffddc4
Ronit Chatterjee <53549804+i-Rony@users.noreply.github.com>
2019-10-13 14:34:40 +0530

    Update Solution.java

--- java-loops-i/Solution.java
+++ java-loops-i/Solution.java
@@ -5,10 +5,11 @@ public class Solution {
 		Scanner sc = new Scanner(System.in);
 
 		int N = sc.nextInt();
-		for (int i = 1; i <= 10; i++) {
-			System.out.println(String.format("%d x %d = %d", N, i, N * i));
-		}
-
+		for (int i = 1; i <= 10; i++)
+        	{
+            		int sum = n * i;
+            		System.out.println(n + " x " + i + " = " + sum);
+        	}
 		sc.close();
 	}
 }
