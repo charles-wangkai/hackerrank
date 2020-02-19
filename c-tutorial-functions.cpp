b16c968cb882d5c1d539c0c4e0f799fbe63f7982
Kai Wang <charles.wangkai@gmail.com>
2020-02-19 15:14:55 +0800

    Functions

--- c-tutorial-functions.cpp
+++ c-tutorial-functions.cpp
@@ -1,21 +1,20 @@
 #include <iostream>
-#include <cstdio>
-using namespace std;
 
-int max(int a, int b) {
-	return a > b ? a : b;
+int max_of_four(int a, int b, int c, int d)
+{
+    return std::max(std::max(a, b), std::max(c, d));
 }
 
-int max_of_four(int a, int b, int c, int d) {
-	return max(max(a, b), max(c, d));
-}
+int main()
+{
+    int a;
+    int b;
+    int c;
+    int d;
+    std::cin >> a >> b >> c >> d;
 
-int main() {
-    int a, b, c, d;
-    scanf("%d %d %d %d", &a, &b, &c, &d);
-    int ans = max_of_four(a, b, c, d);
-    printf("%d", ans);
+    int ans{max_of_four(a, b, c, d)};
+    std::cout << ans << "\n";
 
     return 0;
 }
-
