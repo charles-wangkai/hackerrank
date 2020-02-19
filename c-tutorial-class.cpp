fbc379c6c217b7462936af77b8224b801fe509ec
Kai Wang <charles.wangkai@gmail.com>
2020-02-19 15:58:28 +0800

    Class

--- c-tutorial-class.cpp
+++ c-tutorial-class.cpp
@@ -1,62 +1,79 @@
 #include <iostream>
-#include <sstream>
-using namespace std;
 
-class Student {
+class Student
+{
 private:
 	int age;
-	string first_name;
-	string last_name;
+	std::string first_name;
+	std::string last_name;
 	int standard;
+
 public:
-	int get_age() {
+	int get_age()
+	{
 		return age;
 	}
-	void set_age(int age) {
+
+	void set_age(int age)
+	{
 		this->age = age;
 	}
-	string get_first_name() {
+
+	const std::string &get_first_name()
+	{
 		return first_name;
 	}
-	void set_first_name(string first_name) {
+
+	void set_first_name(const std::string &first_name)
+	{
 		this->first_name = first_name;
 	}
-	string get_last_name() {
+
+	const std::string &get_last_name()
+	{
 		return last_name;
 	}
-	void set_last_name(string last_name) {
+
+	void set_last_name(const std::string &last_name)
+	{
 		this->last_name = last_name;
 	}
-	int get_standard() {
+
+	int get_standard()
+	{
 		return standard;
 	}
-	void set_standard(int standard) {
+
+	void set_standard(int standard)
+	{
 		this->standard = standard;
 	}
-	string to_string() {
-		stringstream ss;
-		ss << age << "," << first_name << "," << last_name << "," << standard;
-		return ss.str();
+
+	std::string to_string()
+	{
+		return std::to_string(age) + "," + first_name + "," + last_name + "," + std::to_string(standard);
 	}
 };
 
-int main() {
-    int age, standard;
-    string first_name, last_name;
+int main()
+{
+	int age;
+	std::string first_name;
+	std::string last_name;
+	int standard;
+	std::cin >> age >> first_name >> last_name >> standard;
 
-    cin >> age >> first_name >> last_name >> standard;
+	Student st;
+	st.set_age(age);
+	st.set_standard(standard);
+	st.set_first_name(first_name);
+	st.set_last_name(last_name);
 
-    Student st;
-    st.set_age(age);
-    st.set_standard(standard);
-    st.set_first_name(first_name);
-    st.set_last_name(last_name);
+	std::cout << st.get_age() << "\n";
+	std::cout << st.get_last_name() << ", " << st.get_first_name() << "\n";
+	std::cout << st.get_standard() << "\n";
+	std::cout << "\n";
+	std::cout << st.to_string() << "\n";
 
-    cout << st.get_age() << "\n";
-    cout << st.get_last_name() << ", " << st.get_first_name() << "\n";
-    cout << st.get_standard() << "\n";
-    cout << "\n";
-    cout << st.to_string();
-
-    return 0;
+	return 0;
 }
