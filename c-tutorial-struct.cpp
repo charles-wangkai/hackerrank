00cdc22043f0d12efebc614ecd7657741b05ce23
Kai Wang <charles.wangkai@gmail.com>
2020-02-19 15:47:24 +0800

    Structs

--- c-tutorial-struct.cpp
+++ c-tutorial-struct.cpp
@@ -1,22 +1,19 @@
-#include <cmath>
-#include <cstdio>
-#include <vector>
 #include <iostream>
-#include <algorithm>
-using namespace std;
 
-struct Student {
-	int age;
-	string first_name;
-	string last_name;
-	int standard;
+struct Student
+{
+    int age;
+    std::string first_name;
+    std::string last_name;
+    int standard;
 };
 
-int main() {
+int main()
+{
     Student st;
+    std::cin >> st.age >> st.first_name >> st.last_name >> st.standard;
 
-    cin >> st.age >> st.first_name >> st.last_name >> st.standard;
-    cout << st.age << " " << st.first_name << " " << st.last_name << " " << st.standard;
+    std::cout << st.age << " " << st.first_name << " " << st.last_name << " " << st.standard;
 
     return 0;
 }
