e788d1ab65005f71bce96795caaf206fc327fa20
Kai Wang <charles.wangkai@gmail.com>
2020-02-19 15:08:35 +0800

    For Loop

--- c-tutorial-for-loop.cpp
+++ c-tutorial-for-loop.cpp
@@ -1,22 +1,28 @@
 #include <iostream>
-#include <cstdio>
-using namespace std;
+#include <array>
 
-int main() {
-	const char* NAMES[] = {NULL, "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+int main()
+{
+	std::array<std::string, 9> names{"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
 
 	int a;
 	int b;
-	scanf("%d%d", &a, &b);
-	for (int n = a; n <= b; n++) {
-		if (n >= 1 && n <= 9) {
-			printf("%s\n", NAMES[n]);
-		} else if (n % 2 == 0) {
-			printf("even\n");
-		} else {
-			printf("odd\n");
+	std::cin >> a >> b;
+	for (int n = a; n <= b; n++)
+	{
+		if (n >= 1 && n <= 9)
+		{
+			std::cout << names[n - 1] << "\n";
+		}
+		else if (n % 2 == 0)
+		{
+			std::cout << "even\n";
+		}
+		else
+		{
+			std::cout << "odd\n";
 		}
 	}
 
-    return 0;
+	return 0;
 }
