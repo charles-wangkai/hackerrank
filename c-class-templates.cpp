f1e3dee4c4e410d65e013e431491d3cc9c4e7513
Kai Wang <charles.wangkai@gmail.com>
2020-02-24 10:30:59 +0800

    C++ Class Templates

---
+++ c-class-templates.cpp
@@ -0,0 +1,71 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <cassert>
+using namespace std;
+
+/*Write the class AddElements here*/
+template <class T>
+class AddElements
+{
+private:
+    T element;
+
+public:
+    AddElements(const T &element) : element{element} {}
+
+    T add(const T &other)
+    {
+        return element + other;
+    }
+};
+
+template <>
+class AddElements<std::string>
+{
+private:
+    std::string element;
+
+public:
+    AddElements(const std::string &element) : element{element} {}
+
+    std::string concatenate(const std::string &other)
+    {
+        return element + other;
+    }
+};
+
+int main()
+{
+    int n, i;
+    cin >> n;
+    for (i = 0; i < n; i++)
+    {
+        string type;
+        cin >> type;
+        if (type == "float")
+        {
+            double element1, element2;
+            cin >> element1 >> element2;
+            AddElements<double> myfloat(element1);
+            cout << myfloat.add(element2) << endl;
+        }
+        else if (type == "int")
+        {
+            int element1, element2;
+            cin >> element1 >> element2;
+            AddElements<int> myint(element1);
+            cout << myint.add(element2) << endl;
+        }
+        else if (type == "string")
+        {
+            string element1, element2;
+            cin >> element1 >> element2;
+            AddElements<string> mystring(element1);
+            cout << mystring.concatenate(element2) << endl;
+        }
+    }
+    return 0;
+}
