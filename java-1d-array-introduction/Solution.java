c418fb24e08e5c57a1bd0d91f95ab2af32f01c64
Kai Wang <charles.wangkai@gmail.com>
2020-07-08 06:14:39 +0800

    Java 1D Array [Fix]

--- java-1d-array-introduction/Solution.java
+++ java-1d-array-introduction/Solution.java
@@ -1,21 +1,23 @@
 import java.util.Scanner;
 
 public class Solution {
+
 	public static void main(String[] args) {
+
 		Scanner scan = new Scanner(System.in);
 		int n = scan.nextInt();
 
 		int[] a = new int[n];
-		for (int i = 0; i < n; i++) {
+		for (int i = 0; i < n; ++i) {
 			int val = scan.nextInt();
 			a[i] = val;
 		}
 
-		// Print all the array elements
+		scan.close();
+
+		// Prints each sequential element in array a
 		for (int i = 0; i < a.length; i++) {
 			System.out.println(a[i]);
 		}
-
-		sc.close();
 	}
 }
