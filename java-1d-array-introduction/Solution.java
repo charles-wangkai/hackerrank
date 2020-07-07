14cde4e93fc6fa2a433e908b21a13245e8328aea
Aditya Thakur <53579386+adityathakurxd@users.noreply.github.com>
2020-07-07 23:19:03 +0530

    Update Solution.java

--- java-1d-array-introduction/Solution.java
+++ java-1d-array-introduction/Solution.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Solution {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+		Scanner scan = new Scanner(System.in);
 		int n = scan.nextInt();
 
 		int[] a = new int[n];
