13ad962c14a7c8efea15f0285291e61c207ce329
saikatdgp2001 <72062968+saikatdgp2001@users.noreply.github.com>
2021-10-01 07:47:44 +0530

    Update Solution.java

--- 30-binary-numbers/Solution.java
+++ 30-binary-numbers/Solution.java
@@ -1,16 +1,18 @@
 import java.util.Scanner;
 
+
 public class Solution {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
-		int n = sc.nextInt();
+		
+		int m = sc.nextInt();
 
-		String binary = Integer.toBinaryString(n);
+		String binary = Integer.toBinaryString(m);
 		int maxConsecutiveOneNum = 0;
 		int consecutiveOneNum = 0;
-		for (int i = 0; i < binary.length(); i++) {
-			if (binary.charAt(i) == '1') {
+		for (int p = 0; p < binary.length(); p++) {
+			if (binary.charAt(p) == '1') {
 				consecutiveOneNum++;
 				maxConsecutiveOneNum = Math.max(maxConsecutiveOneNum, consecutiveOneNum);
 			} else {
